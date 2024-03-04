a4c8478ade9e9b2475f3a24846e69884487f5707
Kiara Grouwstra <kiara@bij1.org>
2024-03-04 12:56:43 +0100

    localize: nl

--- theme/hbs-helpers/date-helpers.js
+++ theme/hbs-helpers/date-helpers.js
@@ -1,9 +1,11 @@
 const moment = require('moment');
 
+const locale = 'nl';
+
 const dateHelpers = {
-  MY: date => moment(date.toString(), ['YYYY-MM-DD']).format('MMM YYYY'),
-  Y: date => moment(date.toString(), ['YYYY-MM-DD']).format('YYYY'),
-  DMY: date => moment(date.toString(), ['YYYY-MM-DD']).format('D MMM YYYY')
+  MY: date => moment(date.toString(), ['YYYY-MM-DD'], locale).format('MMM YYYY'),
+  Y: date => moment(date.toString(), ['YYYY-MM-DD'], locale).format('YYYY'),
+  DMY: date => moment(date.toString(), ['YYYY-MM-DD'], locale).format('D MMM YYYY')
 };
 
 module.exports = { dateHelpers };
